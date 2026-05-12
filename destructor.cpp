355bd633576b3900e7f7c0f79a61669c71c6b623
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-05-12 15:40:12 +0700

    step16

--- destructor.cpp
+++ destructor.cpp
@@ -27,29 +27,3 @@ angka::~angka(){ //Destructor
     cout << "Alamat Array Sudah Dilepaskan" << endl;
 }
 
-void angka::cetakData()
-{
-    for (int i = 1; i <= panjang; i++)
-    {
-        cout  << i << " : " << arr[i] << endl;
-
-    }
-}
-
-void angka::isiData()
-{
-    for (int i = 1; i <= panjang; i++)
-    {
-        cout << i << " : ";
-        cin >> arr[i];
-    }
-    cout << endl;
-}
-int main()
-{
-    angka belajarcpp(3);                    //Constructor Dipanggil
-    angka *ptrBelajarcpp = new angka(5);    //Cosntructor Dipanggil
-    delete ptrBelajarcpp;                   //Destructor Dipanggil
-
-    return 0;    
-}
