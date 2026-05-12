d58901f351b7199d9de0ac5392366170ef2f05e7
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-05-12 15:18:08 +0700

    step2

--- chainfunctioncall.cpp
+++ chainfunctioncall.cpp
@@ -2,3 +2,21 @@
 using namespace std;
 class buku{
     string judul;
+
+    public:
+    buku setjudul(string judul){
+        this->judul = judul;
+        return *this; //chain function
+    }
+string getjudul(){
+    return judul;
+}
+
+};
+
+int main(){
+    buku bukunya;
+    cout << bukunya.setjudul("Matematika").getjudul();  //chain function calls
+    return 0;
+}
+  
--- constructor.cpp
+++ constructor.cpp
@@ -6,5 +6,13 @@ public:
     Mahasiswa(); // constructor
 };
 
+Mahasiswa::Mahasiswa() {
+    cout << "Constructor dipanggil!" << endl; //definisi Constructor
+};
+
+int main() {
+    Mahasiswa mhs; // objek dibuat, constructor otomatis dipanggil
+    return 0;
+}
 
  
