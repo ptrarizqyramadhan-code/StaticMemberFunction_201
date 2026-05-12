774a67221ee490c7de987c7b8f5677420bc98aea
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-05-12 15:44:09 +0700

    step21

--- staticmember.cpp
+++ staticmember.cpp
@@ -19,32 +19,3 @@ public:
     }
 };
 
-//memberi value ke variabel nim dari class Mahasiswa
-int Mahasiswa::nim = 10;
-
-//mengimplementasikan prosedur - prosedur di luar class
-void Mahasiswa::setID() {
-    id = nim++; 
-};
-
-void Mahasiswa::printAll() {
-
-    cout << "ID: " << id << ", Nama: " << nama << endl;
-    cout << "Nama : " << nama << endl;
-    cout << endl;
-};
-
-int main() {
-    //membuat objek dan memberi nilai
-    Mahasiswa mhs1("Lia Kurnia");
-    Mahasiswa mhs2("Asroni");
-    Mahasiswa mhs3("Andi Kurniawan");
-    Mahasiswa mhs4("Joko Purba");
-
-    //memanggil prosedur printAll() 
-    mhs1.printAll();
-    mhs2.printAll();
-    mhs3.printAll();
-
-    return 0;
-}
