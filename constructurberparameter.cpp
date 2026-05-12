1bd8aa6da54edf26a2194ad750ed3f4308392efa
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-05-12 15:38:56 +0700

    step15

--- constructurberparameter.cpp
+++ constructurberparameter.cpp
@@ -2,3 +2,22 @@
 #include <string>
 using namespace std;
 
+class mahasiswa{
+    private:
+        int nim;
+        string nama;
+    public:
+        mahasiswa(int Nim, string Nama); // constructor dengan parameter
+}; 
+
+//Definisi constructor dengan parameter
+mahasiswa::mahasiswa(int Nim, string Nama){
+    cout << "Constructor dengan parameter terpanggil" << endl;
+    cout << "NIM : " << Nim << endl;
+    cout << "Nama : " << Nama << endl;
+}
+
+int main(){
+    mahasiswa mhs(112345, "Pascal"); //memanggil constructor dengan parameter
+    return 0;
+}
