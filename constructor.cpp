62752c85de7d2f0531a7991a061b5bf20251e62e
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-05-12 15:21:50 +0700

    step6

--- constructor.cpp
+++ constructor.cpp
@@ -1,18 +1,5 @@
 #include <iostream>
 using namespace std;
 
-class Mahasiswa {
-public:
-    Mahasiswa(); // constructor
-};
-
-Mahasiswa::Mahasiswa() {
-    cout << "Constructor dipanggil!" << endl; //definisi Constructor
-};
-
-int main() {
-    Mahasiswa mhs; // objek dibuat, constructor otomatis dipanggil
-    return 0;
-}
 
  
