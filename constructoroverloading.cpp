27e4040dfd2b2b33b7069b8b36d05a66468a8993
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-05-12 15:27:43 +0700

    step11

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -35,3 +35,14 @@ void Mahasiswa::cetak(){
     cout << "Nama : " << nama << endl;
 }
 
+int main(){
+    Mahasiswa mhs1 (102030);
+    Mahasiswa mhs2("andi");
+    Mahasiswa mhs3(010203,"Santi"); 
+   
+    //tampilkan nilai
+    cout << "Mahasiswa 1 : "; mhs1.cetak();
+    cout << "Mahasiswa 2 : "; mhs2.cetak();
+    cout << "Mahasiswa 3 : "; mhs3.cetak();
+    return 0;
+}
