932ae37467637a8c475caeb1057edf7f0683a2fe
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-05-12 15:26:28 +0700

    step10

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -13,3 +13,19 @@ class Mahasiswa{
     Mahasiswa(int iNim, string iNama);
     void cetak();
 };
+Mahasiswa::Mahasiswa(){
+}
+
+Mahasiswa::Mahasiswa(int iNim){
+    nim = iNim; //definisi hanya NIM
+}
+
+Mahasiswa::Mahasiswa(string iNama){
+    nama = iNama; //definisi hanya Nama
+}
+
+Mahasiswa::Mahasiswa(int iNim, string iNama){
+    nim = iNim; //definisi NIM dan Nama
+    nama = iNama;
+}
+
