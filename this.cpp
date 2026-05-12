e5b6a1cbc0950d5df8747d024678fa4bd1d399fb
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-05-12 15:46:15 +0700

    step25

--- this.cpp
+++ this.cpp
@@ -6,4 +6,17 @@ class buku
 
     string judul;
 
+public:
+    string setget (string judul)
+    {
+        this->judul = judul; //nilai parameter
+        return this->judul;
+    }
+};
 
+int main()
+{
+    buku bukunya;
+    cout << "Judul Buku : " << bukunya.setget("Matematika");
+    return 0;
+}
